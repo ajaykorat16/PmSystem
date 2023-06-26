16869f7c576f64ce433a93c814982ed7911b6ed1
ParthilShekhada <shekhadaparthil@gmail.com>
2023-06-26 12:15:47 +0530

    Manage Login

--- client/src/contexts/JWTContext.js
+++ client/src/contexts/JWTContext.js
@@ -109,13 +109,13 @@ function AuthProvider({ children }) {
   }, []);
 
   const login = async (email, password) => {
-    const response = await axios.post('/api/account/login', {
+    const response = await axios.post('/user/login', {
       email,
       password,
     });
-    const { accessToken, user } = response.data;
+    const { token, user } = response.data;
 
-    setSession(accessToken);
+    setSession(token);
     dispatch({
       type: 'LOGIN',
       payload: {
