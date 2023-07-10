4c2d3786fe5c36848cec57f657771a46fb7ef798
Ajay Korat <ajaykorat16@gmail.com>
2023-07-10 12:06:24 +0530

    Mange Photo

--- client/src/components/AppHeaderDropdown.js
+++ client/src/components/AppHeaderDropdown.js
@@ -1,61 +1,71 @@
-import React from 'react'
-import {
-  CAvatar,
-  CDropdown,
-  CDropdownDivider,
-  CDropdownHeader,
-  CDropdownItem,
-  CDropdownMenu,
-  CDropdownToggle,
-} from '@coreui/react'
-import {
-  cilLockLocked,
-  cilUser,
-} from '@coreui/icons'
-import CIcon from '@coreui/icons-react'
-
-import avatar8 from './../assets/images/avatars/8.jpg'
-import { useNavigate } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'
-import { useUser } from '../context/UserContext'
-import { useState, useEffect } from 'react'
-import { CImage } from '@coreui/react'
-
+import React, { useState, useEffect } from 'react';
+import { CImage, CAvatar, CDropdown, CDropdownDivider, CDropdownHeader, CDropdownItem, CDropdownMenu, CDropdownToggle } from '@coreui/react';
+import { cilLockLocked, cilUser } from '@coreui/icons';
+import CIcon from '@coreui/icons-react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+import { useUser } from '../context/UserContext';
 
 const AppHeaderDropdown = () => {
-  const [photo, setPhoto] = useState("");
-  const { logout, auth } = useAuth()
-  const { getUserProfile } = useUser()
-  const navigate = useNavigate()
+  const [photo, setPhoto] = useState('');
+  const { logout, auth } = useAuth();
+  const { getUserProfile } = useUser();
+  const [getAuth, setGetAuth] = useState(null);
+  const [isPhoto, setIsPhoto] = useState(false)
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     try {
-      logout()
-      navigate('/login')
+      logout();
+      navigate('/login');
     } catch (error) {
-      console.log(error)
+      console.log(error);
+    }
+  };
+
+  const handleLogin = () => {
+    try {
+      navigate('/login');
+    } catch (error) {
+      console.log(error);
     }
   }
 
   const fetchData = async () => {
     try {
-      if (auth?.user) {
-      let { getProfile } = await getUserProfile(auth.user._id);
-      setPhoto(getProfile.photo);
-    }
+      if (getAuth && getAuth.user) {
+        const { getProfile } = await getUserProfile(getAuth?.user._id);
+        setPhoto(getProfile.photo);
+        setIsPhoto(true)
+      }
     } catch (error) {
-      console.error("Error fetching user profile:", error);
+      console.error('Error fetching user profile:', error);
     }
   };
 
   useEffect(() => {
-      fetchData();
+    let authdata = JSON.parse(localStorage.getItem('auth'));
+    setGetAuth(authdata);
   }, []);
-  
+
+  useEffect(() => {
+    if (getAuth && getAuth.user) {
+      fetchData();
+    }
+  }, [getAuth]);
+
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
-        <CImage align="start" rounded src={photo} width={30} height={30} />
+        {isPhoto &&
+          <CImage align="start" rounded src={photo} width={30} height={30} alt="User Avatar" />
+        }
+        {isPhoto === false &&
+          <CAvatar color="success" textColor="white" shape="rounded">
+            PM
+          </CAvatar>
+        }
+
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-light fw-semibold py-2">Settings</CDropdownHeader>
@@ -64,13 +74,19 @@ const AppHeaderDropdown = () => {
           Profile
         </CDropdownItem>
         <CDropdownDivider />
-        <CDropdownItem onClick={handleLogout} >
-          <CIcon icon={cilLockLocked} className="me-2" />
-          {auth ? ("Logout"): ("Login")}
-        </CDropdownItem>
+        {getAuth ?
+          <CDropdownItem onClick={handleLogout}>
+            <CIcon icon={cilLockLocked} className="me-2" />
+            Logout
+          </CDropdownItem> :
+          <CDropdownItem onClick={handleLogin}>
+            <CIcon icon={cilLockLocked} className="me-2" />
+            Login
+          </CDropdownItem>
+        }
       </CDropdownMenu>
     </CDropdown>
-  )
-}
+  );
+};
 
-export default AppHeaderDropdown
+export default AppHeaderDropdown;
