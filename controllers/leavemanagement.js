fefe2faf2b473e891afcb30819bf1253ca11bea8
Ajay Korat <ajaykorat16@gmail.com>
2023-08-25 13:43:14 +0530

    Create manage leave

--- controllers/leavemanagement.js
+++ controllers/leavemanagement.js
@@ -1,6 +1,9 @@
 const LeaveManagement = require("../models/leaveManagementModel");
-const asyncHandler = require("express-async-handler");
 const Users = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const moment = require('moment');
+const { parsedDate } = require("../helper/mail")
+const { validationResult } = require("express-validator");
 
 const getLeavesMonthWise = asyncHandler(async (req, res) => {
   try {
@@ -102,4 +105,31 @@ const getUserLeaves = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getLeavesMonthWise, getSingleLeave, updateLeave, getUserLeaves };
+const createManageLeave = asyncHandler(async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: true, errors: errors.array() });
+  }
+  try {
+    const { user, monthly, leave } = req.body
+
+    const today = new Date()
+    const currentYear = today.getFullYear()
+    const monthlyDate = moment(new Date(currentYear, monthly - 1, 1)).format('YYYY-MM-DD');
+
+    const manageCreateLeave = await new LeaveManagement({ user, monthly: parsedDate(monthlyDate), leave }).save()
+    await Users.findByIdAndUpdate(user, { $inc: { leaveBalance: leave } }, { new: true })
+
+    return res.status(201).json({
+      error: false,
+      message: "Create Manage Leave Successfully.",
+      manageLeave: manageCreateLeave
+    })
+
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Server error");
+  }
+})
+
+module.exports = { getLeavesMonthWise, getSingleLeave, updateLeave, getUserLeaves, createManageLeave };
--- routers/leaveManagement.js
+++ routers/leaveManagement.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router();
 const { auth, isAdmin } = require("../middleware/auth");
-const { getLeavesMonthWise, getSingleLeave, updateLeave, getUserLeaves } = require('../controllers/leavemanagement');
+const { check } = require('express-validator');
+
+const { getLeavesMonthWise, getSingleLeave, updateLeave, getUserLeaves, createManageLeave } = require('../controllers/leavemanagement');
 
 router.get("/", auth, isAdmin, getLeavesMonthWise)
+
+router.post("/create-manageLeave",
+    check('user', 'User is reruired').notEmpty(),
+    check('monthly', 'Month is reruired').notEmpty(),
+    check('leave', 'Leave is reruired').notEmpty(),
+    auth, isAdmin, createManageLeave)
 
 router.post("/search", auth, isAdmin, getLeavesMonthWise)
 
