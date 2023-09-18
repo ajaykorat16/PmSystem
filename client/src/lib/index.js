45a16dfd9c0744a3a1e096deab6e9294aafc2329
Ajay Korat <ajaykorat16@gmail.com>
2023-09-18 19:04:16 +0530

    manange prob URL

--- client/src/lib/index.js
+++ client/src/lib/index.js
@@ -1,5 +1,5 @@
 const dev = "http://localhost:8080";
-const prod = "http://65.2.169.255:8080/";
+const prod = "http://65.2.169.255:8080";
 
 export const baseURL =
   window.location.hostname.split(":")[0] === "localhost" ||
