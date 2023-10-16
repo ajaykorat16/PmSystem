1084d770bf67351878d295f0942a90113a2a2dae
Ajay Korat <ajaykorat16@gmail.com>
2023-10-16 18:40:47 +0530

    Manage All the function for credential

--- controllers/credentials.js
+++ controllers/credentials.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
-const Projects = require("../models/projects");
-const Users = require("../models/userModel");
-const Worklog = require("../models/workLogmodel");
 const Credential = require("../models/credentials")
 const asyncHandler = require("express-async-handler");
 const { validationResult } = require("express-validator");
-const { capitalizeFLetter, formattedDate } = require("../helper/mail");
+const { capitalizeFLetter } = require("../helper/mail");
 
 const createCredential = asyncHandler(async (req, res) => {
     const errors = validationResult(req);
@@ -17,7 +14,6 @@ const createCredential = asyncHandler(async (req, res) => {
         const { title, description, users } = req.body;
 
         const existingCredential = await Credential.findOne({ title: capitalizeFLetter(title) });
-
         if (existingCredential) {
             return res.status(400).json({
                 error: true,
@@ -43,4 +39,127 @@ const createCredential = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { createCredential }
+const getCredential = asyncHandler(async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 10;
+        const sortField = req.query.sortField || "createdAt";
+        const sortOrder = req.query.sortOrder || -1;
+        const { filter } = req.body;
+
+        let query = { "users.id": req.user._id };
+
+        if (filter) {
+            query.$or = [
+                { title: { $regex: filter, $options: "i" } }
+            ];
+        }
+
+        const totalCredentialCount = await Credential.countDocuments(query);
+
+        const credential = await Credential.find(query).skip((page - 1) * limit).limit(limit).sort({ [sortField]: sortOrder }).lean();
+        return res.status(201).json({
+            error: false,
+            message: "Credential Get Successfully",
+            credential,
+            currentPage: page,
+            totalPages: Math.ceil(totalCredentialCount / limit),
+            totalCredential: totalCredentialCount,
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error");
+    }
+});
+
+const getSingleCredential = asyncHandler(async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const project = await Credential.findById(id).populate({ path: "users.id", select: "-photo", });
+        return res.status(200).json({
+            error: false,
+            message: "Single Credential Get Successfully.",
+            project,
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error");
+    }
+});
+
+const updateCredential = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const { title, description, users } = req.body;
+        const { id } = req.params;
+
+        const existingCredential = await Credential.findById(id);
+        if (!existingCredential) {
+            return res.status(404).json({
+                error: true,
+                message: "This credential is not existing in the database.",
+            });
+        }
+
+        if (existingCredential.title === capitalizeFLetter(title)) {
+            return res.status(400).json({
+                error: true,
+                message: "Credential with the same title already exists.",
+            });
+        }
+
+        const projectObj = {
+            title: title ? capitalizeFLetter(title) : existingCredential.title,
+            description: description ? capitalizeFLetter(description) : existingCredential.description,
+            users: users || existingCredential.users,
+        };
+
+        if (users && Array.isArray(users)) {
+            const newUserIds = users.map((p) => {
+                return { id: new mongoose.Types.ObjectId(p) };
+            });
+            projectObj.users = [{ id: req.user._id }].concat(newUserIds);
+        }
+
+        const updatedCredential = await Credential.findByIdAndUpdate(id, projectObj, { new: true, });
+
+        return res.status(200).json({
+            error: false,
+            message: "Credential updated successfully.",
+            credential: updatedCredential,
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error");
+    }
+});
+
+const deleteCredential = asyncHandler(async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const existingCredential = await Credential.findById(id);
+        if (!existingCredential) {
+            return res.status(404).json({
+                error: true,
+                message: "This credential is not existing in the database.",
+            });
+        }
+
+        await Credential.findByIdAndDelete(id);
+        return res.status(200).json({
+            error: false,
+            message: "Credential is delete successfully.",
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error");
+    }
+});
+
+module.exports = { createCredential, getCredential, getSingleCredential, updateCredential, deleteCredential }
--- routers/credentials.js
+++ routers/credentials.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { check } = require('express-validator');
-const { auth, isAdmin } = require("../middleware/auth")
+const { auth } = require("../middleware/auth")
 
-const { createCredential } = require("../controllers/credentials")
+const { createCredential, getCredential, getSingleCredential, updateCredential, deleteCredential } = require("../controllers/credentials")
+
+router.get("/", auth, getCredential)
+
+router.get("/single-credential/:id", auth, getSingleCredential)
 
 router.post("/create",
     check('title', 'Title is required').notEmpty(),
     check('description', 'Description is required').notEmpty(),
     auth, createCredential)
+
+router.put("/update/:id", auth, updateCredential)
+
+router.delete("/delete/:id", auth, deleteCredential)
 
 module.exports = router
