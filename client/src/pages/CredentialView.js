aeb43af10f4d6a1bb9780acf13268917c5782c91
Ajay Korat <ajaykorat16@gmail.com>
2023-10-17 16:35:18 +0530

    manage credentials view

--- client/src/pages/CredentialView.js
+++ client/src/pages/CredentialView.js
@@ -5,6 +5,7 @@ import { useAuth } from '../context/AuthContext';
 import { ScrollPanel } from 'primereact/scrollpanel';
 import Layout from "./Layout";
 import '../styles/Styles.css'
+import { Avatar } from "primereact/avatar";
 
 function CredentialView({ title }) {
     const { id } = useParams()
@@ -13,43 +14,55 @@ function CredentialView({ title }) {
     const [credentialDetail, setCredentialDetail] = useState({
         title: "",
         description: "",
-        users: ""
+        users: "",
+        photo: "",
     })
     const navigate = useNavigate()
 
     useEffect(() => {
         const fetchCredential = async () => {
             const data = await getSingleCredential(id);
+            console.log(data);
             if (data) {
                 const userNames = data.users.map(user => user.id.fullName);
+                const userPhotos = data.users.map(user => user.id.photo)
+                console.log(userPhotos);
                 setCredentialDetail({
                     title: data.title,
                     description: data.description,
-                    users: userNames.join(", ")
+                    users: userNames.join(", "),
+                    photo: userPhotos
                 });
             }
         };
         fetchCredential();
     }, [getSingleCredential, id]);
 
+    console.log(credentialDetail.photo);
+
     return (
         <Layout title={title}>
-            <div className="mainBody credential">
-                <p className="credentialTitle">{credentialDetail.title}</p>
-                <ScrollPanel
-                    className="customCredential"
-                >
-                    <div className="credentialDescription" dangerouslySetInnerHTML={{ __html: credentialDetail.description }}></div>
-                </ScrollPanel>
-                <div className="credentialUsers">
-                    <p className="users">{credentialDetail.users}</p>
+            <div className="m-2 credential">
+                <div className="row">
+                    <div className="col-11 credentialBody">
+                        <p className="credentialTitle">{credentialDetail.title}</p>
+                        <div className="credentialDescription" dangerouslySetInnerHTML={{ __html: credentialDetail.description }}></div>
+                        <button
+                            className="btn btn-primary mt-2"
+                            onClick={() => { auth.user.role === "admin" ? navigate('/dashboard/credential/list') : navigate('/dashboard-user/credential/list') }}
+                        >
+                            Back
+                        </button>
+                        {/* <p className="users">{credentialDetail.users}</p> */}
+                    </div>
+                    <div className="col-1">
+                        {credentialDetail.photo ? (
+                            <Avatar image={`${credentialDetail.photo}`} size="xlarge" shape="circle" />
+                        ) : (
+                            <Avatar icon="pi pi-user" className="avatar" size="large" shape="circle" />
+                        )}
+                    </div>
                 </div>
-                <button
-                    className="btn btn-primary mt-2"
-                    onClick={() => { auth.user.role === "admin" ? navigate('/dashboard/credential/list') : navigate('/dashboard-user/credential/list') }}
-                >
-                    Back
-                </button>
             </div>
         </Layout>
     )
