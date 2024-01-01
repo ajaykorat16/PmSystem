54510c684e81215489d3b931aa33d6ea479750e9
Ajay Korat <ajaykorat16@gmail.com>
2024-01-01 11:30:13 +0530

    Manage cron schedule of Monthly cron

--- routers/cron.js
+++ routers/cron.js
@@ -2,12 +2,12 @@ const cron = require("node-cron");
 
 const { carryForwardLeaves, createMonthly } = require("../controllers/cron")
 
-//run every year - 1 Jan
+//run every year - 1 Jan 00:00
 cron.schedule("0 0 1 1 *", () => {
     carryForwardLeaves();
 });
 
-//run every month
-cron.schedule("0 0 1 * *", () => {
+//run every month - 00:05
+cron.schedule("5 0 1 * *", () => {
     createMonthly();
 });
