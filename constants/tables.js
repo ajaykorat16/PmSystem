b41a16d3572c1a999431fbc3b219a1a5e7dd0f43
Ajay Korat <ajaykorat16@gmail.com>
2024-05-20 16:26:25 +0530

    Constant database table names

---
+++ constants/tables.js
@@ -0,0 +1,21 @@
+const USERS = 'users';
+const DEPARTMENTS = 'departments';
+const LEAVES = 'leaves';
+const LEAVEMANAGEMENTS = 'leavemanagements';
+const PROJECTS = 'projects';
+const CREDENTIALS = 'credentials';
+const WORKLOGS = 'worklogs';
+const USER_PROJECT_RELATION = 'user_project_relation';
+const USER_CREDENTIAL_RELATION = 'user_credential_relation'
+
+module.exports =  {
+    USERS,
+    DEPARTMENTS,
+    LEAVES,
+    LEAVEMANAGEMENTS,
+    PROJECTS,
+    CREDENTIALS,
+    WORKLOGS,
+    USER_PROJECT_RELATION,
+    USER_CREDENTIAL_RELATION,
+};
