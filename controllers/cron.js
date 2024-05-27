8ec1a323aa57c64cceae738d7e18a179d3b08c5c
Ajay Korat <ajaykorat16@gmail.com>
2024-05-27 17:16:32 +0530

    Manage Cron

--- controllers/cron.js
+++ controllers/cron.js
@@ -1,48 +1,36 @@
-const Users = require("../models/userModel")
-const Leaves = require("../models/leaveModel")
-const LeaveManagement = require("../models/leaveManagementModel")
+const { USERS, LEAVEMANAGEMENTS, LEAVES } = require("../constants/tables");
+const { knex } = require('../database/db');
 
 const carryForwardLeaves = async () => {
     try {
-        const allUsers = await Users.find({ role: "user" }).select("_id fullName carryForward");
+        const allUsers = await knex.select('u.id', 'u.fullName', 'u.carryForward', 'u.leaveBalance').from(`${USERS} as u`).where('role', 'user')
         const currentDate = new Date();
         const oneYearAgo = new Date(currentDate.getFullYear() - 1, currentDate.getMonth(), currentDate.getDate());
-        for (const user of allUsers) {
-            const getLeaves = await Leaves.aggregate([
-                {
-                    $match: {
-                        userId: user._id,
-                        status: "approved",
-                        startDate: {
-                            $gte: oneYearAgo
-                        }
-                    }
-                },
-                {
-                    $group: {
-                        _id: '$userId',
-                        totalLeaves: { $sum: '$totalDays' },
-                    },
-                },
-            ]);
 
-            const previousYearLeaves = await LeaveManagement.aggregate([
-                {
-                    $match: { user: user._id }
-                },
-                {
-                    $group: {
-                        _id: '$user',
-                        leave: { $sum: '$leave' },
-                    },
-                },
-            ]);
+        for(const user of allUsers) {
+            const getLeaves = await knex(`${LEAVES} as l`)
+                .select('l.userId', knex.raw('SUM(l.totalDays) as totalLeaves'))
+                .where('l.userId', user.id)
+                .andWhere('l.status', 'approved')
+                .andWhere('l.leaveType', 'paid')
+                .andWhere('l.startDate', '>=', oneYearAgo)
+                .groupBy('l.userId');
+            
+            const previousYearLeaves = await knex(`${LEAVEMANAGEMENTS} as lm`)
+                .select('lm.user', knex.raw('SUM(lm.leave) as totalLeave'))
+                .where('lm.user', user.id)
+                .andWhere('lm.monthly', '>=', oneYearAgo)
+                .groupBy('lm.user');
 
             for (const lastLeaves of previousYearLeaves) {
                 for (const leave of getLeaves) {
-                    const finalTotal = (user.carryForward + lastLeaves.leave) - leave.totalLeaves;
+                    const finalTotal = (parseFloat(user.carryForward) + parseFloat(lastLeaves.totalLeave)) - parseFloat(leave.totalLeaves);
                     const carryForwardLeave = finalTotal >= 5 ? 5 : (finalTotal >= 0 ? finalTotal : 0);
-                    await Users.findByIdAndUpdate(user._id, { $set: { carryForward: carryForwardLeave, leaveBalance: carryForwardLeave } });
+                    await knex(USERS).where('id', user.id).update({ 
+                        carryForward: carryForwardLeave, 
+                        leaveBalance: carryForwardLeave,
+                        updatedAt: new Date()
+                    });
                 }
             }
         }
@@ -56,16 +44,18 @@ const createMonthly = async () => {
     try {
         let today = new Date();
         let leave = 1.5
-        const allUsers = await Users.find({ role: "user" }).select("_id, fullName, carryForward")
+
+        const allUsers = await knex(USERS).where('role', 'user');
+
         allUsers.map(async (e) => {
-            await new LeaveManagement({ user: e._id, monthly: today, leave }).save()
-            await Users.findByIdAndUpdate(e._id, { $inc: { leaveBalance: leave } }, { new: true })
+            await knex(LEAVEMANAGEMENTS).insert({ user: e.id, monthly: today, leave });
+            
+            await knex(USERS).where('id', e.id).increment('leaveBalance', leave).update({ updatedAt: new Date() });
         })
     } catch (error) {
         console.log(error);
     }
-
 }
-//createMonthly()
+// createMonthly()
 
 module.exports = { carryForwardLeaves, createMonthly }
